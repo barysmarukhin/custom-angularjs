b6eb53f4914389d55cc553ab37c5fbbfa4f031bc
Barys_Marukhin <Barys_Marukhin@epam.com>
2018-05-14 16:10:09 +0300

    Chapter 1. NaNs

--- src/scope.js
+++ src/scope.js
@@ -50,7 +50,10 @@ Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
     if (valueEq) {
         return _.isEqual(newValue, oldValue);
     } else {
-        return newValue === oldValue;
+        return (newValue === oldValue ||
+            typeof newValue === 'number' && typeof oldValue === 'number' &&
+            isNaN(newValue) && isNaN(oldValue)
+        );
     }
 };
 
